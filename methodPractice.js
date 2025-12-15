8868918e11399b86a43f242266c9cab0c3a32bba
Suman Kumar <sumanqaj9876@gmail.com>
2025-12-15 23:01:23 +0530

    feat: add object utility methods and higher order functions examples

--- methodPractice.js
+++ methodPractice.js
@@ -25,3 +25,52 @@ let result = str1
   .join(" ");
 console.log(result);
 
+// rest operator
+let obj = { a: 1, b: 2, c: 3 }
+let { a, ...rest } = obj
+console.log(a)
+console.log(rest)
+
+// Object Utility Methods
+let user={ name: "suman", age: 23, city: "patna" }
+console.log(Object.keys(user))
+console.log(Object.values(user))
+console.log(Object.entries(user))
+console.log(Object.freeze(user))
+user.age=24
+console.log(Object.seal(user))
+user.age=24
+console.log(user)
+delete user.city
+
+
+// optional chaining
+let user = {
+  name: "suman",
+  address: {  
+    city: "patna",
+    
+  }
+}
+console.log(user?.address?.city)
+console.log(user?.contact?.phone)
+
+//practice
+let user = { name: "suman", age: 23, city: "patna" };
+for (let [key, value] of Object.entries(user)) {
+  console.log(key+":"+value);
+}
+
+
+//higher order function , callback function 
+function operate(a, b, fn) {
+  return fn(a, b);
+}
+function add(a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return a - b;
+}
+console.log(operate(10, 5, add));
+console.log(operate(10, 5, sub));
