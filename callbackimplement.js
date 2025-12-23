1ebb3669768f3476a0036160f046eb51ff1d7475
Suman Kumar <sumanqaj9876@gmail.com>
2025-12-23 23:46:19 +0530

    feat: implement calculator HTML structure and refactor callback implementation to promises

---
+++ callbackimplement.js
@@ -0,0 +1,92 @@
+// // Implementation using callback hell
+// const cart = ["shirt", "pants", "tshirt"];
+// function createOrder(cart, cb) {
+//   setTimeout(() => {
+//     if (cart.length === 0) {
+//       console.log("Cart is Empty!!");
+//       return;
+//     }
+//     const orderID = "ORD123";
+//     console.log("Order Created");
+//     console.log(orderID);
+//     cb(orderID);
+//   }, 2000);
+// }
+// function proceedtoPayment(orderID, cb) {
+//   setTimeout(function () {
+//     console.log("Payment done for : ", orderID);
+//     cb("Payment_Success");
+//   }, 1000);
+// }
+// function showOrderSummary(paymentInfo, cb) {
+//   setTimeout(() => {
+//     console.log("Order Summary shown : ", paymentInfo);
+//     cb();
+//   }, 1000);
+// }
+// function updateWallet() {
+//   setTimeout(() => {
+//     console.log("Wallet updated");
+//   }, 3000);
+// }
+// createOrder(cart, function (orderID) {
+//   proceedtoPayment(orderID, function (paymentInfo) {
+//     showOrderSummary(paymentInfo, function (paymentInfo) {
+//       updateWallet();
+//     });
+//   });
+// });
+
+
+
+
+
+
+ //USING PROMISES
+ let cart = ["shirt", "pants", "tshirt"];
+function createOrder(cart) {
+  return new Promise((resolve, reject) => {
+    if (!validateCart()) {
+      reject(new Error("Cart is empty"));
+    }
+    setTimeout(function () {
+      console.log("Order created and order id is : ORD123");
+      resolve("ORD123");
+    }, 2000);
+  });
+}
+function validateCart() {
+  return true;
+}
+function proceedToPayment(orderID) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Payment Success");
+      resolve("Payment_Success");
+    }, 3000);
+  });
+}
+function showOrderSummary(paymentInfo) {
+  return new Promise((resolve) => {
+    console.log("Order Summary shown: ", paymentInfo);
+    resolve();
+  });
+}
+function updateWallet() {
+  console.log("Updated Wallet");
+}
+
+
+createOrder(cart)
+  .then((orderID) => {
+    return proceedToPayment(orderID);
+  })
+  .then((paymentInfo) => {
+    return showOrderSummary(paymentInfo);
+  })
+  .then(() => {
+    updateWallet();
+  })
+  .catch((err) => {
+    console.log("Error", err);
+  });
---
+++ promise.js
@@ -0,0 +1,18 @@
+const promise = new Promise((resolve, reject) => { 
+    let success = false; 
+if(success){
+    resolve("Operation was successful!");
+
+}
+else{
+    reject("Operation failed.");
+}
+});
+
+promise
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => {
+    console.error(error);
+});
